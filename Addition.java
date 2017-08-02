71933d23c96536f9b81efba28b43b358a74ec2da
Manjunath027 <30576657+Manjunath027@users.noreply.github.com>
2017-08-02 15:17:07 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+
+public class ADDITION {
+
+  public static void main(String a[]) 
+    {
+        int x=3;
+        int y=3;
+        int z = x+y;
+        int w = x*y;
+    System.out.println(z);
+    System.out.println(w);
+    }
+    
+}
