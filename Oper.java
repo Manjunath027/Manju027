17857c15c7c9b7f787a69ccaa9f4b05f9c3830b9
Manjunath027 <30576657+Manjunath027@users.noreply.github.com>
2017-08-04 14:24:55 +0530

    Add files via upload

---
+++ Oper.java
@@ -0,0 +1,21 @@
+class Oper
+{
+
+public static void main (String args[])
+{
+
+int a = 10;
+int b = 15;
+boolean x = true;
+boolean y = false;
+int c;
+boolean d;
+
+c = a&b;
+d = x&&y;
+
+System.out.println(c);
+System.out.println(d);
+}
+
+}
