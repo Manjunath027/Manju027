9f2f130d90114f41f7f51e9897849d730152fa68
Manjunath027 <30576657+Manjunath027@users.noreply.github.com>
2017-08-05 13:51:47 +0530

    Add files via upload

---
+++ dhara.java
@@ -0,0 +1,28 @@
+class dhara
+{
+
+public static void main(String args[])
+{
+
+int a = 10;
+
+int b;
+
+double c = 10.2;
+
+float d;
+
+int e;
+
+d = (float)c; 
+
+b = (int)c;
+
+e = (int)a;
+
+System.out.println(d);
+System.out.println(b);
+System.out.println(e);
+}
+
+}
